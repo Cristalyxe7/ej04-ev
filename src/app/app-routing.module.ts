8a32ff5cb85a5584c2e3a6b07b291a9b2da596e7
Adrian Tagirta <alexmola917@gmail.com>
2023-01-29 12:32:15 +0100

    Version 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './secciones/error404/error404.component';
+import { FabricantesComponent } from './secciones/fabricantes/fabricantes.component';
+import { InicioComponent } from './secciones/inicio/inicio.component';
+import { VehiculosComponent } from './secciones/vehiculos/vehiculos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'vehiculos', component: VehiculosComponent},
+  {path: 'fabricantes', component: FabricantesComponent},
+  {path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  {path: '**', component: Error404Component}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavComponent } from './barra-nav/barra-nav.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { InicioComponent } from './secciones/inicio/inicio.component';
+import { VehiculosComponent } from './secciones/vehiculos/vehiculos.component';
+import { FabricantesComponent } from './secciones/fabricantes/fabricantes.component';
+import { Error404Component } from './secciones/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavComponent,
+    CabeceraComponent,
+    InicioComponent,
+    VehiculosComponent,
+    FabricantesComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
